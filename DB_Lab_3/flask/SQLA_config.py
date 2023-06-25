03f5d932b8a389849d4bf76357fc2dde4c538956
Rostislav3 <55854722+Rostislav3@users.noreply.github.com>
2023-06-25 16:04:07 +0300

    4 labs

---
+++ DB_Lab_3/flask/SQLA_config.py
@@ -0,0 +1,261 @@
+import json
+
+from sqlalchemy import create_engine, MetaData, func, distinct
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import select
+import redis
+
+# 'postgresql+psycopg2://user:password@hostname/database_name'
+engine = create_engine("postgresql+psycopg2://postgres:root@db/postgresdb")
+
+
+r = redis.Redis(host='redis', port=6379)
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+#zno_records = Base.classes.zno_records
+Students = Base.classes.students
+Locations = Base.classes.locations
+EO = Base.classes.educational_organisations
+Tests = Base.classes.tests_results
+
+
+
+
+conn = engine.connect()
+#result = conn.execute(query)
+
+"""
+Methods for Locations table
+"""
+
+
+def fetchRowsFromLocations():
+    query = select(Locations).order_by(Locations.location_id).limit(10)
+    return conn.execute(query)
+
+
+def fetchLocationsById(location_id):
+    query = session.query(Locations).filter(Locations.location_id == location_id)
+    return query.first()
+
+
+def fetchRegnames():
+    query = select(distinct(Locations.regname))
+    return conn.execute(query)
+
+
+def fetchLocation(regname, areaname, tername, tertypename):
+    query = session.query(Locations).filter(
+        Locations.regname == regname,
+        Locations.areaname == areaname,
+        Locations.tername == tername,
+        Locations.tertypename == tertypename
+    )
+    return query.first()
+
+
+def deleteLocation(location_id):
+    location = fetchLocationsById(location_id)
+    if location:
+        session.delete(location)
+        session.commit()
+
+
+def updateLocation(location_id, regname, areaname, tername, tertypename):
+    location = fetchLocationsById(location_id)
+    if location:
+        location.regname = regname
+        location.areaname = areaname
+        location.tername = tername
+        location.tertypename = tertypename
+        session.commit()
+
+
+"""
+Methods for Students table
+"""
+
+
+def fetchRowsFromStudents():
+    query = select(Students).order_by(Students.student_id).limit(10)
+    return conn.execute(query)
+
+
+def fetchStudentById(student_id):
+    query = session.query(Students).filter(Students.student_id == student_id)
+    return query.first()
+
+
+def createStudent(data):
+    student = Students(year_of_passing=data['year_of_passing'], outid=data['outid'], birth=data['birth'],
+                       sextypename=data['sextypename'], location_id=data['location_id'], eo_id=data['eo_id'])
+    session.add(student)
+    session.commit()
+
+
+def updateStudent(student_id, year_of_passing, outid, birth, sextypename, location_id, eo_id, tests_results_id):
+    student = fetchStudentById(student_id)
+    if student:
+        student.year_of_passing = year_of_passing
+        student.outid = outid
+        student.birth = birth
+        student.sextypename = sextypename
+        student.location_id = location_id
+        student.eo_id = eo_id
+        student.tests_results_id = tests_results_id
+        session.commit()
+
+def deleteStudent(outid):
+    student = fetchStudentById(outid)
+    if student:
+        session.delete(student)
+        session.commit()
+
+
+"""
+Methods for Educational organizations table
+"""
+
+
+def fetchRowsFromEO():
+    query = select(EO).order_by(EO.eo_id).limit(10)
+    return conn.execute(query)
+
+
+def fetchEOById(eo_id):
+    query = session.query(EO).filter(EO.eo_id == eo_id, EO.eo_name != 'NaN')
+    return query.first()
+
+
+def fetchEO(eo_name, eo_type, location_id):
+    query = session.query(EO).filter(
+        EO.eo_name == eo_name,
+        EO.eo_type == eo_type,
+        EO.location_id == location_id
+    )
+    return query.first()
+
+
+def deleteEO(eo_name, eo_type, location_id):
+    eo = fetchEO(eo_name, eo_type, location_id)
+    if eo:
+        session.delete(eo)
+        session.commit()
+
+
+def updateEO(eo_id, eo_name, eo_type, location_id):
+    eo = fetchEOById(eo_id)
+    if eo:
+        eo.eo_name = eo_name
+        eo.eo_type = eo_type
+        eo.location_id = location_id
+        session.commit()
+
+
+"""
+Methods for Tests Results table
+"""
+
+
+def fetchRowsFromTests():
+    query = select(Tests).order_by(Tests.tests_id).limit(10)
+    return conn.execute(query)
+
+
+def fetchTestsColumnNames():
+    return Tests.__table__.columns.keys()
+
+
+def fetchTest(tests_id):
+    query = session.query(Tests).filter(Tests.tests_id == tests_id)
+    return query.first()
+
+
+def updateTest(tests_id, test_data):
+    test = fetchTest(tests_id)
+    if test:
+        for column_name, value in test_data.items():
+            if value == 'None':
+                value = None
+            setattr(test, column_name, value)
+        session.commit()
+
+
+def deleteTest(student_id):
+    test = fetchTest(student_id)
+    if test:
+        session.delete(test)
+        session.commit()
+
+
+"""
+Different queries
+"""
+
+
+
+def fetchGrade(year, regname, subject, function):
+    cache_key = f"grade:{year}:{regname}:{subject}:{function}"
+
+    # Перевірка, чи дані є в кеші
+    cached_result = r.get(cache_key)
+
+    if cached_result is not None:
+        # Повернення результату з кешу
+        return json.loads(cached_result)
+
+    if regname != "м.Київ":
+        regname += " область"
+
+    query = session.query(
+        Students.year_of_passing,
+        Locations.regname,
+        getattr(func, function)(getattr(Tests, subject))
+    ).join(
+        Locations, Students.location_id == Locations.location_id
+    ).join(
+        Tests, Students.tests_results_id == Tests.tests_id
+    ).filter(
+        getattr(Tests, subject.split("_")[0] + "_test_status") == 'Зараховано',
+        Students.year_of_passing == year,
+        Locations.regname == regname
+    ).group_by(
+        Locations.regname, Students.year_of_passing
+    )
+
+    print(query.all())
+    grade = 0
+    if len(query.all()) != 0:
+        grade = query.all()[0][2]
+        r.setex(cache_key, 7200, float(grade))
+        return grade
+
+    r.setex(cache_key, 7200, float(grade))
+    return grade
+
+
+def subjectDict():
+    return {"Українська мова та література": "uml_test_ball100",
+                     "Українська мова": "ukr_test_ball100",
+                     "Історія України": "hist_test_ball100", "Математика": "math_test_ball100",
+                     "Фізика": "phys_test_ball100", "Хімія": "chem_test_ball100",
+                     "Географія": "geo_test_ball100",
+                     "Англійська мова": "eng_test_ball100", "Французька мова": "fr_test_ball100",
+                     "Німецька мова": "deu_test_ball100", "Іспанська мова": "sp_test_ball100"}
+
+
+def spaceProblemSolverDict():
+    return {"Українська_мова_та_література": "Українська мова та література",
+     "Українська_мова":"Українська мова",
+     "Історія": "Історія України", "Математика": "Математика",
+     "Фізика": "Фізика", "Хімія": "Хімія",
+     "Географія": "Географія",
+     "Англійська": "Англійська мова", "Французька": "Французька мова",
+     "Німецька": "Німецька мова", "Іспанська": "Іспанська мова"}
---
+++ DB_Lab_4/flask/app.py
@@ -0,0 +1,243 @@
+from flask import Flask, render_template, url_for, redirect, request
+from pymongo import MongoClient
+
+import SQLA_config, mongo_config
+from mongo_config import *
+import psycopg2
+
+app = Flask(__name__)
+
+#engine = create_engine("postgresql+psycopg2://postgres:root1@localhost/student01_DB")
+engine = create_engine("postgresql+psycopg2://postgres:root@db/postgresdb")
+
+#mongo_client = MongoClient('mongodb://localhost:27017/')
+mongo_client = MongoClient('mongodb://mongodb:27017/')
+
+db = mongo_client.student01_DB
+
+# docker-compose build --no-cache && docker-compose up -d --force-recreate
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Config = SQLA_config.Config
+
+
+@app.route("/", methods=['POST', 'GET'])
+def showTables():
+    global Config
+    config = Config()
+    if request.method == 'POST':
+        if request.form["submit_button"] == "Use PostgreSQL":
+            Config = SQLA_config.Config
+        if request.form["submit_button"] == "Use MongoDB":
+            Config = mongo_config.Config
+
+        print(Config)
+        if request.form["submit_button"] == "Show locations":
+            headers = ("location_id", "regname", "areaname", "tername", "tertypename")
+            data = tuple(config.fetchRowsFromLocations())
+            return render_template("table.html", headers=headers, data=data, url="/locations")
+
+        elif request.form['submit_button'] == "Show students":
+            headers = ("student_id", "year_of_passing", "outid", "birth", "sextypename", "location_id", "eo_id",
+                       "tests_results_id")
+            data = tuple(config.fetchRowsFromStudents())
+            return render_template("table.html", headers=headers, data=data, url="/students")
+
+        elif request.form['submit_button'] == "Show EO":
+            headers = ("eo_id", "eo_name", "eo_type", "location_id")
+            data = tuple(config.fetchRowsFromEO())
+            return render_template("table.html", headers=headers, data=data, url="/eo")
+
+        elif request.form['submit_button'] == "Show Tests":
+            headers = config.fetchTestsColumnNames()
+            data = tuple(config.fetchRowsFromTests())
+            return render_template("table.html", headers=headers, data=data, url="/tests")
+
+        elif request.form['submit_button'] == "Filters":
+            years = ("2019", "2021")
+            regnames = config.fetchRegnames()
+            subjects_dict = config.subjectDict()
+            subjects = list(subjects_dict.keys())
+            index = 0
+            for subject in subjects:
+                if subjects[index] == 'Українська мова':
+                    subjects[index] = 'Українська_мова'
+
+                if subjects[index] == 'Українська мова та література':
+                    subjects[index] = 'Українська_мова_та_література'
+                index += 1
+
+            subjects = tuple(subjects)
+            functions = ('max', 'min', 'avg')
+
+            return render_template("filters.html", years=years, regnames=regnames, subjects=subjects,
+                                   functions=functions, url="/filters")
+
+    return render_template("main.html")
+
+
+@app.route("/locations", methods=['POST', 'GET'])
+def locationsTable():
+    global Config
+    config = Config()
+    url = "/locations"
+    headers = ("location_id", "regname", "areaname", "tername", "tertypename")
+    data = tuple(config.fetchRowsFromLocations())
+    if request.method == 'POST':
+        location_id = request.form['location_id']
+        regname = request.form['regname']
+        areaname = request.form['areaname']
+        tername = request.form['tername']
+        tertypename = request.form['tertypename']
+        if 'update_delete' in request.form:
+            if request.form['update_delete'] == "Update":
+                config.updateLocation(location_id, regname, areaname, tername, tertypename)
+                print(request.form)
+            if request.form['update_delete'] == "Delete":
+                config.deleteLocation(location_id)
+
+        else:
+            config.createLocation(location_id, regname, areaname, tername, tertypename)
+
+        session.commit()
+        return redirect(url)
+
+    return render_template("table.html", headers=headers, data=data, url=url)
+
+
+@app.route("/eo", methods=['POST', 'GET'])
+def eoTable():
+    global Config
+    config = Config()
+
+    url = "/eo"
+    headers = ("eo_id", "eo_name", "eo_type", "location_id")
+    data = tuple(config.fetchRowsFromEO())
+    if request.method == 'POST':
+        eo_id = request.form['eo_id']
+        eo_name = request.form['eo_name']
+        eo_type = request.form['eo_type']
+        location_id = request.form['location_id']
+        if 'update_delete' in request.form:
+            if request.form['update_delete'] == "Update":
+                config.updateEO(eo_id, eo_name, eo_type, location_id)
+
+            if request.form['update_delete'] == "Delete":
+                config.deleteEO(eo_id)
+
+        else:
+            config.createEO(eo_id, eo_name, eo_type, location_id)
+
+        session.commit()
+        return redirect(url)
+
+    return render_template("table.html", headers=headers, data=data, url=url)
+
+
+@app.route("/students", methods=['POST', 'GET'])
+def studentsTable():
+    global Config
+    config = Config()
+
+    url = "/students"
+    headers = (
+    "student_id", "year_of_passing", "outid", "birth", "sextypename", "location_id", "eo_id", "tests_results_id")
+    data = tuple(config.fetchRowsFromStudents())
+    if request.method == 'POST':
+        student_id = request.form['student_id']
+        year_of_passing = request.form['year_of_passing']
+        outid = request.form['outid']
+        birth = request.form['birth']
+        sextypename = request.form['sextypename']
+        location_id = request.form['location_id']
+        eo_id = request.form['eo_id']
+        tests_results_id = request.form['tests_results_id']
+        if 'update_delete' in request.form:
+            if request.form['update_delete'] == "Update":
+                config.updateStudent(student_id, year_of_passing, outid, birth, sextypename, location_id, eo_id,
+                                     tests_results_id)
+
+            if request.form['update_delete'] == "Delete":
+                config.deleteStudent(outid)
+                session.commit()
+
+        else:
+            config.createStudent(student_id, year_of_passing, outid, birth, sextypename, location_id, eo_id,
+                                 tests_results_id)
+
+        session.commit()
+        return redirect(url)
+
+    return render_template("table.html", headers=headers, data=data, url=url)
+
+
+@app.route("/tests", methods=['POST', 'GET'])
+def testsTable():
+    global Config
+    config = Config()
+
+    url = "/tests"
+    headers = tuple(config.fetchTestsColumnNames())
+    data = tuple(config.fetchRowsFromTests())
+    if request.method == 'POST':
+        tests_data = {column_name: request.form.get(column_name) if request.form.get(column_name) != '' else None for
+                      column_name in headers}
+        test = Tests(**tests_data)
+        if 'update_delete' in request.form:
+            if request.form['update_delete'] == "Update":
+                config.updateTest(test.tests_id, tests_data)
+
+            if request.form['update_delete'] == "Delete":
+                config.deleteTest(test.student_id)
+
+        else:
+            config.createTest(tests_data)
+        session.commit()
+        return redirect(url)
+
+    return render_template("table.html", headers=headers, data=data, url=url)
+
+
+@app.route("/filters", methods=["POST", "GET"])
+def filters():
+    config = SQLA_config.Config()
+
+    years = ("2019", "2021")
+    regnames = config.fetchRegnames()
+    subjects_dict = config.subjectDict()
+    subjects = list(subjects_dict.keys())
+    index = 0
+    for subject in subjects:
+        if subjects[index] == 'Українська мова':
+            subjects[index] = 'Українська_мова'
+
+        if subjects[index] == 'Українська мова та література':
+            subjects[index] = 'Українська_мова_та_література'
+        index += 1
+
+    subjects = tuple(subjects)
+    functions = ('max', 'min', 'avg')
+    if request.method == 'POST':
+        selected_year = request.form['years']
+        selected_regname = request.form['regnames']
+        selected_subject = request.form['subjects']
+        selected_function = request.form['funcs']
+
+        for key in config.spaceProblemSolverDict().keys():
+            if selected_subject == key:
+                selected_subject = config.spaceProblemSolverDict().get(key)
+        query_result = config.fetchGrade(selected_year, selected_regname, subjects_dict.get(selected_subject),
+                                         selected_function)
+        grade = query_result
+        if grade == 0:
+            grade = 'None'
+        session.commit()
+
+    return render_template("filters.html", years=years, regnames=regnames, subjects=subjects, functions=functions,
+                           grade=grade, url="/filters")
+
+
+if __name__ == "__main__":
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
